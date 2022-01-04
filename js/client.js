659cf9830ef9451888005bade4a5e2461dceb69b
Will Haight <willhaight121@gmail.com>
2022-01-04 10:58:34 -0700

    Removed unneccessary files and made it functional

--- js/client.js
+++ js/client.js
@@ -1,11 +1,44 @@
 const net = require('net');
+let name = "unamed";
+let currentName = "unamed";
+let nameChange = false;
+let firstMessage = true;
 
-let client = net.createConnection({ port: 3000 }, () => {
+let client = net.createConnection({ port: 3001 }, () => {
     console.log('Client Connected');
-
-    const input =
-
-        input.on('data', (data) => {
-            console.log(data);
-        })
+    client.on('data', (data) => {
+        if (firstMessage == true) {
+            console.log(`type /username`);
+            firstMessage = false;
+        } else if (firstMessage == false) {
+            if (data.toString().trim() == "/taken") {
+                console.log('That name is currently unavailable')
+                name = "unamed";
+            } else {
+                console.log(`${data}`);
+            }
+        }
+    });
+    process.stdin.on('data', (data) => {
+        if (data.toString().trim() == "/username") {
+            currentName = name;
+            client.write("/newname");
+            console.log('type new name.');
+            nameChange = true;
+        } else if (nameChange == true) {
+            name = data.toString().trim();
+            console.log(`Your name is now ${name}`);
+            let newArr = [];
+            newArr.push(data.toString().trim());
+            newArr.push(currentName);
+            console.log(`I am newArr ${newArr}`)
+            client.write(newArr.toString());
+            nameChange = false;
+        } else if (name == "unamed") {
+            console.log("You must set a username before chatting. /username to begin.");
+        }
+        else {
+            client.write(`${name + ': ' + data.toString().trim()}`)
+        }
+    });
 });
--- js/server.js
+++ js/server.js
@@ -1,11 +1,55 @@
 const net = require('net');
-
+const { EventEmitter } = require('stream');
+let contactList = [];
+let nameList = [];
+let updateNameList = false;
+let count = 0;
+let messageHandler;
+let nameTaken = false;
+let myEmitter = new EventEmitter();
 let server = net.createServer(client => {
     console.log('server is started');
-
+    contactList.push(`${client}`)
+    client.write(contactList[count]);
+    count++;
     client.on('data', (data) => {
-        console.log(data);
-    })
+        if (data == "/newname") {
+            updateNameList = true;
+        } else if (updateNameList == true) {
+            let newData = data[1]
+            console.log(newData)
+            for (let i = 0; i < (nameList.length + 1); i++) {
+                if (newData[0] == nameList[i]) {
+                    console.log('Taken name attempt');
+                    nameTaken = true;
+                }
+                if (newData[1] == nameList[i]) {
+                    nameList[i] = null;
+                }
+            }
+            if (nameTaken == true) {
+                client.write('/taken');
+                nameTaken = false;
+            } else {
+                nameList.push(data.toString().trim());
+                updateNameList = false;
+            }
+        } else {
+            console.log(`${data.toString().trim()}`);
+            messageHandler = `${data.toString().trim()}`;
+            myEmitter.emit('message');
+        }
+    });
 
-}).listen(3000);
+    myEmitter.on('message', () => { client.write(messageHandler) });
+
+    process.stdin.on('data', (data) => {
+        if (data.toString().trim() == "/list") {
+            console.log(nameList)
+        } else {
+            client.write(`${'server: ' + data.toString().trim()}`)
+        }
+    });
+
+}).listen(3001);
 
